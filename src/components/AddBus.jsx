73c96b9a77e8b5a20d9fbb93e327ae15a3e05e61
Liyo08 <liyotom04@gmail.com>
2024-06-29 00:21:35 +0530

    navbar edit

--- src/components/AddBus.jsx
+++ src/components/AddBus.jsx
@@ -30,7 +30,7 @@ const AddBus = () => {
 <input type="text" className="form-control" />
     </div>
     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-        <button className="btn btn-success">Submit</button>
+        <button className="btn btn-danger">Submit</button>
     </div>
 </div>
 
